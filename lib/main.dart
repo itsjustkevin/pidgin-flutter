a99c2e4187acd59d878d934785107e83c3ecb2d8
itsjustkevin <kevinjchisholm@gmail.com>
2021-10-11 22:22:54 -0500

    remove streambuilder from dashboard page to stop app from crashing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -1,16 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class DashboardView extends StatefulWidget {
-  const DashboardView({ Key? key }) : super(key: key);
+  const DashboardView({Key? key}) : super(key: key);
 
   @override
   _DashboardViewState createState() => _DashboardViewState();
 }
 
 class _DashboardViewState extends State<DashboardView> {
+  final Stream<QuerySnapshot> _gamesStream =
+      FirebaseFirestore.instance.collection('games').snapshots();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +22,28 @@ class _DashboardViewState extends State<DashboardView> {
         decoration: const BoxDecoration(color: Colors.white),
         child: const Center(
           child: Text("Dashboard"),
+          // child: StreamBuilder<QuerySnapshot>(
+          //     stream: _gamesStream,
+          //     builder: (BuildContext context,
+          //         AsyncSnapshot<QuerySnapshot> snapshot) {
+          //       if (!snapshot.hasData) {
+          //         return const Center(
+          //           child: CircularProgressIndicator(),
+          //         );
+          //       }
+
+          //       return ListView(
+          //         children:
+          //             snapshot.data!.docs.map((DocumentSnapshot document) {
+          //           Map<String, dynamic> data =
+          //               document.data()! as Map<String, dynamic>;
+          //           return ListTile(
+          //             title: Text(data['name']),
+          //             subtitle: Text(data['image']),
+          //           );
+          //         }).toList(),
+          //       );
+          //     }),
         ),
       ),
     );
